42788a02fbb70bc71e1d7a494263a6fb72ad6356
ALESSANDRA CARVALHO DA SILVA <alessandra.csilva68@senacsp.edu.br>
2023-06-30 16:47:13 -0300

    pokedex atualizada

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,33 @@
-const listaSelecaoPokemons = document.querySelector('.pokemon')
-const pokemonsCard = document.querySelector('.pokemon-card')
+const listaSelecaoPokemons = document.querySelectorAll('.pokemon')
+const pokemonsCard = document.querySelectorAll('.pokemon-card')
 
-listaSelecaoPokemons.addEventListener.forEach(pokemon => {
-    
+ 
+
+listaSelecaoPokemons.forEach(pokemon => {
     pokemon.addEventListener('click', () => {
-        const cartaoPokemonAberto = document.querySelector('.open')
-        cartaoPokemonAberto.classList.remove('.open')
+        const cartaoPokemonAberto = document.querySelector('.open')        
+        cartaoPokemonAberto.classList.remove('open')
+
+ 
 
         const idPokemonSelecionado = pokemon.attributes.id.value
+
+ 
+
+        const idDoCartaoPokemonParaAbrir = idPokemonSelecionado  + '-card'
+        const cartaoPokemonParaAbrir = document.getElementById(idDoCartaoPokemonParaAbrir)
+        cartaoPokemonParaAbrir.classList.add('open')
+
+ 
+
+        const pokemonAtivoNaListagem = document.querySelector('.ativo')
+        pokemonAtivoNaListagem.classList.remove('ativo')
+
+ 
+
+        const pokemonSelecionadoNaListagem = document.getElementById(idPokemonSelecionado)
+        pokemonSelecionadoNaListagem.classList.add('ativo')
+ 
+
     })
-});
+})
